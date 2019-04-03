d70bbbd9ca1513f4672dd116108a852072177a70
Tyler Swartz <tmswartz12@gmail.com>
2019-04-03 12:34:20 -0400

    feat: initialized playByPlay in redux store

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,10 @@ import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import gamecast from './gamecast'
+import playByPlay from './playByPlay'
 import footer from './footer'
 
-const reducer = combineReducers({ gamecast, footer })
+const reducer = combineReducers({ gamecast, footer, playByPlay })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
@@ -13,4 +14,5 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './gamecast'
+export * from './playByPlay'
 export * from './footer'
