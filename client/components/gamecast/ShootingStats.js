e39e554e90c9cf1aa7414e0f2a8d519cf50d54a8
Tyler Swartz <tmswartz12@gmail.com>
2019-04-03 12:15:57 -0400

    style: added prop to dictate team styling

--- client/components/gamecast/ShootingStats.js
+++ client/components/gamecast/ShootingStats.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const ShootingStats = props => {
+  console.log(props)
   return (
     <div className="shooting-stats-column">
       {props.shootingStats.map((stat, idx) => {
         return (
           <div key={idx} className="shooting-stats shooting-stats-border">
             <div>{`${stat.title}`}</div>
-            <div className="home stat">{`${stat.perc}`}</div>
+            <div className={`${props.team} stat`}>{`${stat.perc}`}</div>
           </div>
         )
       })}
