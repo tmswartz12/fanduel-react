7a4fbce1eb5ee246f6a7670f09110ca9cc0d2f20
Tyler Swartz <tmswartz12@gmail.com>
2019-04-02 22:39:03 -0400

    feat: navbar created. fanduel logo not rendering

---
+++ client/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="nav-bar">
+      <img
+        className="fanduel-logo"
+        alt="FanDuel Logo"
+        src="../../public/logo.png"
+      />
+      <nav className="nav-menu-left">
+        <div className="nav-link-container">
+          <div className="nav-link">Lobby</div>
+        </div>
+        <div className="nav-link-container">
+          <div className="nav-link">Upcoming</div>
+        </div>
+        <div className="nav-link-container">
+          <div className="nav-link">
+            Live <i className="fas fa-angle-down" />
+          </div>
+        </div>
+        <div className="nav-link-container">
+          <div className="nav-link nav-link-last">History</div>
+        </div>
+      </nav>
+
+      <nav className="nav-menu-right">
+        <div className="nav-link-container">
+          <div className="nav-link">
+            Help <i className="fas fa-angle-down" />
+          </div>
+        </div>
+        <div className="nav-link-container">
+          <div className="nav-link nav-link-last">
+            <i
+              className="fas fa-user-circle"
+              style={{ fontSize: '20px', paddingRight: '10px' }}
+            />
+            {`nbonaddio `}
+            <i className="fas fa-angle-down" />
+          </div>
+        </div>
+        <div className="balance-container">
+          <div className="remaining-balance bold">$1,000,000.00</div>
+          <div className="remaining-balance">Balance</div>
+        </div>
+        <div className="btn-add-funds">Add Funds</div>
+      </nav>
+      <div className="hamburger">
+        <i className="fas fa-bars" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
