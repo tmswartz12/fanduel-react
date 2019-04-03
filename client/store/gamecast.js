9691be6ce4acf140d18823f767f239d447529e8b
Tyler Swartz <tmswartz12@gmail.com>
2019-04-03 11:28:24 -0400

    feat: initialized gamecast redux functionality

---
+++ client/store/gamecast.js
@@ -0,0 +1,88 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_GAMECAST = 'GET_GAMECAST'
+
+/**
+ * INITIAL STATE
+ */
+const defaultGamecast = {
+  game_id: 0,
+  arena: '',
+  location: '',
+  quarter: 0,
+  time_left: '',
+  away_team: {
+    abbrev: '',
+    name: '',
+    location: '',
+    shooting_stats: [
+      {
+        title: '',
+        perc: ''
+      },
+      {
+        title: '',
+        perc: ''
+      },
+      {
+        title: '',
+        perc: ''
+      }
+    ],
+    score: 0
+  },
+  home_team: {
+    abbrev: '',
+    name: '',
+    location: '',
+    shooting_stats: [
+      {
+        title: '',
+        perc: ''
+      },
+      {
+        title: '',
+        perc: ''
+      },
+      {
+        title: '',
+        perc: ''
+      }
+    ],
+    score: 81
+  }
+}
+
+/**
+ * ACTION CREATORS
+ */
+const getGamecast = gamecast => ({ type: GET_GAMECAST, gamecast })
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchGamecast = () => async dispatch => {
+  try {
+    const { data } = await axios.get(
+      'https://my-json-server.typicode.com/fanduel/moneyball-fe-challenge-data/Gamecast_scoreboard'
+    )
+    dispatch(getGamecast(data || defaultGamecast))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultGamecast, action) {
+  switch (action.type) {
+    case GET_GAMECAST:
+      return action.gamecast
+    default:
+      return state
+  }
+}
--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import gamecast from './gamecast'
 import footer from './footer'
 
-const reducer = combineReducers({ footer })
+const reducer = combineReducers({ gamecast, footer })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
